d53323154f103f69555b490f5a9513a80c853b7e
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-03-02 23:40:46 +0100

    Task 1 - urlWriter

---
+++ src/main/java/org/futurecollars/lesson12/task1/Main.java
@@ -0,0 +1,10 @@
+package org.futurecollars.lesson12.task1;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        ReaderURL.saveUrlToFile("dataUrl2", "src/main/resources", "https://guitarcenter.pl/catalog/");
+
+    }
+}
