ba42b958c431f9c598e57ad72ebf302918b3c9ab
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-03-05 20:33:40 +0100

    Task 2

---
+++ src/main/java/org/futurecollars/lesson12/task4/MathUtils.java
@@ -0,0 +1,32 @@
+package org.futurecollars.lesson12.task4;
+
+import java.util.List;
+
+public class MathUtils {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Runnable firstTask = () -> {
+            double average = numbers.stream()
+                    .mapToInt(Integer::intValue)
+                    .average()
+                    .orElse(Double.NaN);
+            System.out.println("Average" + average);
+        };
+
+        Runnable secondTask = () -> {
+            double sum = numbers.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+            System.out.println("Sum" + sum);
+        };
+
+        Thread firstThread = new Thread(firstTask);
+        Thread secondThread = new Thread(secondTask);
+
+        firstThread.start();
+        secondThread.start();
+
+    }
+}
